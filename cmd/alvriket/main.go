512f891f38b88f90d51aebd068b4ea202e37bed6
Robin Eklind <rnd0x00@gmail.com>
2019-07-21 15:23:20 +0200

    alvriket: add protobuf for ELF parser

--- cmd/alvriket/main.go
+++ cmd/alvriket/main.go
@@ -1,15 +1,31 @@
+//go:generate protoc -I ../../alvriket --go_out=plugins=grpc:../../alvriket ../../alvriket/alvriket.proto
+
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
 	"log"
+	"net"
 	"os"
 
 	"github.com/decomp/exp/bin"
 	_ "github.com/decomp/exp/bin/elf" // register ELF decoder
 	"github.com/kr/pretty"
+	"github.com/lapsang-boys/pippi/alvriket"
+	"github.com/mewkiz/pkg/term"
 	"github.com/pkg/errors"
+	"google.golang.org/grpc"
+)
+
+var (
+	// dbg is a logger with the "alvriket:" prefix which logs debug messages to
+	// standard error.
+	dbg = log.New(os.Stderr, term.CyanBold("alvriket:")+" ", 0)
+	// warn is a logger with the "alvriket:" prefix which logs warning messages
+	// to standard error.
+	warn = log.New(os.Stderr, term.RedBold("alvriket:")+" ", 0)
 )
 
 func usage() {
@@ -29,33 +45,74 @@ Flags:
 func main() {
 	// Parse command line arguments.
 	var (
-		// Path to binary executable.
-		binPath string
+		// Address to listen on for gRPC.
+		grpcAddr string
 	)
 	flag.Usage = usage
+	flag.StringVar(&grpcAddr, "addr", "", `address to listen on for gRPC (e.g. ":50051")`)
 	flag.Parse()
-	switch flag.NArg() {
-	case 0:
-		// Read from standard input.
-		binPath = "-"
-	case 1:
-		binPath = flag.Arg(0)
-	default:
-		flag.Usage()
-		os.Exit(1)
-	}
 
-	if err := parseELF(binPath); err != nil {
-		log.Fatalf("%+v", err)
+	switch {
+	case len(grpcAddr) != 0:
+		// Launch grpc server.
+		if err := listen(grpcAddr); err != nil {
+			log.Fatalf("%+v", err)
+		}
+	default:
+		// Path to binary executable.
+		var binPath string
+		switch flag.NArg() {
+		case 0:
+			// Read from standard input.
+			binPath = "-"
+		case 1:
+			binPath = flag.Arg(0)
+		default:
+			flag.Usage()
+			os.Exit(1)
+		}
+
+		// Parse ELF file.
+		file, err := bin.ParseFile(binPath)
+		if err != nil {
+			log.Fatalf("unable to parse ELF file %q: %+v", binPath, err)
+		}
+
+		// Pretty-print to standard output.
+		pretty.Println(file)
 	}
 }
 
-// parseELF parses the given ELF file.
-func parseELF(binPath string) error {
-	file, err := bin.ParseFile(binPath)
+// listen listens on the given gRPC address for incoming requests to parse ELF
+// files.
+func listen(grpcAddr string) error {
+	dbg.Printf("listening on %q", grpcAddr)
+	l, err := net.Listen("tcp", grpcAddr)
 	if err != nil {
 		return errors.WithStack(err)
 	}
-	pretty.Println(file)
+	s := grpc.NewServer()
+	alvriket.RegisterELFParserServer(s, &elfParserServer{})
+	if err := s.Serve(l); err != nil {
+		return errors.WithStack(err)
+	}
 	return nil
+}
+
+// elfParserServer implements alvriket.ELFParserServer.
+type elfParserServer struct{}
+
+// ParseELF parses the given ELF file.
+func (elfParserServer) ParseELF(ctx context.Context, req *alvriket.ParseELFRequest) (*alvriket.ParseELFReply, error) {
+	dbg.Printf("parsing %q", req.ElfPath)
+	// Parse ELF file.
+	file, err := bin.ParseFile(req.ElfPath)
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+	// Send reply.
+	reply := &alvriket.ParseELFReply{
+		Nsects: int32(len(file.Sections)),
+	}
+	return reply, nil
 }
--- go.mod
+++ go.mod
@@ -4,12 +4,15 @@ go 1.13
 
 require (
 	github.com/decomp/exp v0.0.0-20181210173419-052a5b074939
+	github.com/golang/protobuf v1.3.2
 	github.com/kr/pretty v0.1.0
+	github.com/mewkiz/pkg v0.0.0-20190317153131-e8616647c365
 	github.com/pkg/errors v0.8.1
+	github.com/sanctuary/djavul v0.0.0-20190720091136-28857b228999 // indirect
 	golang.org/x/net v0.0.0-20190628185345-da137c7871d7 // indirect
 	golang.org/x/sys v0.0.0-20190712062909-fae7ac547cb7 // indirect
 	golang.org/x/text v0.3.2 // indirect
 	google.golang.org/appengine v1.4.0 // indirect
 	google.golang.org/genproto v0.0.0-20190716160619-c506a9f90610 // indirect
-	google.golang.org/grpc v1.22.0 // indirect
+	google.golang.org/grpc v1.22.0
 )
