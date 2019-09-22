bb8b69804e536f21c64be3e8895197dc17736f4c
Henry Eklind <henry@karlek.io>
2019-09-22 22:12:22 +0200

    feature(Frontend/bindings): add new bindings
    - Bindings now return raw protobuf replies
    - Add new bindings: sections, disassembly, binary

--- cmd/pippi/client.go
+++ cmd/pippi/client.go
@@ -5,12 +5,14 @@ import (
 	"log"
 	"time"
 
+	binpb "github.com/lapsang-boys/pippi/proto/bin"
+	disasmpb "github.com/lapsang-boys/pippi/proto/disasm"
 	stringspb "github.com/lapsang-boys/pippi/proto/strings"
 	"github.com/pkg/errors"
 	"google.golang.org/grpc"
 )
 
-func Strings(addr, binId string) ([]string, error) {
+func Strings(addr, binId string) ([]*stringspb.StringInfo, error) {
 	// Connect to gRPC server.
 	conn, err := grpc.Dial(addr, grpc.WithInsecure())
 	if err != nil {
@@ -32,9 +34,55 @@ func Strings(addr, binId string) ([]string, error) {
 	}
 	log.Println(time.Since(now))
 
-	rawStrings := []string{}
-	for _, s := range reply.Strings {
-		rawStrings = append(rawStrings, s.RawString)
+	return reply.Strings, nil
+}
+
+func Sections(addr, binId string) (*binpb.File, error) {
+	// Connect to gRPC server.
+	conn, err := grpc.Dial(addr, grpc.WithInsecure())
+	if err != nil {
+		return nil, errors.WithStack(err)
 	}
-	return rawStrings, nil
+	defer conn.Close()
+
+	// Send binary parsing request.
+	client := binpb.NewBinaryParserClient(conn)
+	ctx := context.Background()
+
+	req := &binpb.ParseBinaryRequest{
+		BinId: binId,
+	}
+	now := time.Now()
+	file, err := client.ParseBinary(ctx, req)
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+	log.Println(time.Since(now))
+
+	return file, nil
+}
+
+func Disassembly(addr, binId string) (*disasmpb.DisassembleReply, error) {
+	// Connect to gRPC server.
+	conn, err := grpc.Dial(addr, grpc.WithInsecure())
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+	defer conn.Close()
+
+	// Send binary parsing request.
+	client := disasmpb.NewDisassemblerClient(conn)
+	ctx := context.Background()
+
+	req := &disasmpb.DisassembleRequest{
+		BinId: binId,
+	}
+	now := time.Now()
+	reply, err := client.Disassemble(ctx, req)
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+	log.Println(time.Since(now))
+
+	return reply, nil
 }
--- cmd/pippi/main.go
+++ cmd/pippi/main.go
@@ -3,17 +3,64 @@
 package main
 
 import (
+	"io/ioutil"
 	"log"
+	"os"
+	"path/filepath"
 
 	"github.com/leaanthony/mewn"
 	"github.com/wailsapp/wails"
+
+	binpb "github.com/lapsang-boys/pippi/proto/bin"
+	disasmpb "github.com/lapsang-boys/pippi/proto/disasm"
+	stringspb "github.com/lapsang-boys/pippi/proto/strings"
 )
 
-func basic(binId string) []string {
-	extractedStrings, err := Strings("localhost:1234", binId)
+func binary(binId string) []byte {
+	const (
+		ext = ".bin"
+	)
+
+	cacheDir, err := os.UserCacheDir()
 	if err != nil {
 		log.Println(err)
-		return []string{}
+		return []byte{}
+	}
+	cacheDir = filepath.Join(cacheDir, "pippi")
+	// Read file contents.
+	binName := binId + ext
+	binPath := filepath.Join(cacheDir, binId, binName)
+	binData, err := ioutil.ReadFile(binPath)
+	if err != nil {
+		log.Println(err)
+		return []byte{}
+	}
+	return binData
+}
+
+func sections(binId string) *binpb.File {
+	file, err := Sections("localhost:1200", binId)
+	if err != nil {
+		log.Println(err)
+		return nil
+	}
+	return file
+}
+
+func disassembly(binId string) *disasmpb.DisassembleReply {
+	reply, err := Disassembly("localhost:1300", binId)
+	if err != nil {
+		log.Println(err)
+		return nil
+	}
+	return reply
+}
+
+func strings(binId string) []*stringspb.StringInfo {
+	extractedStrings, err := Strings("localhost:1400", binId)
+	if err != nil {
+		log.Println(err)
+		return []*stringspb.StringInfo{}
 	}
 	return extractedStrings
 }
@@ -30,6 +77,9 @@ func main() {
 		CSS:    css,
 		Colour: "#131313",
 	})
-	app.Bind(basic)
+	app.Bind(binary)
+	app.Bind(disassembly)
+	app.Bind(sections)
+	app.Bind(strings)
 	app.Run()
 }
