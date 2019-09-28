f5080ff8b08112fb0bbbc5892c7ba8594ca4fa30
Henry Eklind <henry@karlek.io>
2019-09-28 15:09:40 +0200

    feature(SelectId): add component select-id

---
+++ cmd/pippi/frontend/src/app/select-id/select-id.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { IdService } from '../id.service';
+
+@Component({
+  selector: 'app-select-id',
+  templateUrl: './select-id.component.html',
+  styleUrls: ['./select-id.component.css']
+})
+export class SelectIdComponent {
+  ids: string[] = [];
+
+  currentId: string = "";
+  constructor(
+    private idService: IdService,
+  ) {
+    //@ts-ignore
+    window.backend.listIds().then(ids => {
+      this.ids = ids;
+    })
+    this.idService.id.subscribe(newId => {
+      this.currentId = newId
+    });
+  }
+
+  short(id: string): string {
+    // For 1 thousand binaries, there's a 2.93% probability of collision.
+    return id.substring(0, 6);
+  }
+
+  setId(id: string): void {
+    this.idService.set(id);
+  }
+}
