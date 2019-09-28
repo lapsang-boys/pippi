69967ec89a186a775983fe9b03803c4eb20b18c6
Henry Eklind <henry@karlek.io>
2019-09-28 15:12:33 +0200

    feature(app): update app design

--- cmd/pippi/frontend/src/app/app.component.ts
+++ cmd/pippi/frontend/src/app/app.component.ts
@@ -1,60 +1,50 @@
-import { Component, OnInit } from '@angular/core';
-import { Buffer } from "buffer";
-import { FileUploader } from 'ng2-file-upload/ng2-file-upload';
-import  hexdump from "hexdump-nodejs";
-
-const uploadURL = 'http://localhost:2000/upload'
+import { Component } from '@angular/core';
+import { BinaryService } from './binary.service';
+import { DisassemblyService } from './disassembly.service';
+import { IdService } from './id.service';
+import { LoaderService } from './loader.service';
 
 @Component({
   selector: '[id="app"]',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'Pippi front-end';
 
-  public uploader: FileUploader = new FileUploader({url: uploadURL, itemAlias: 'file'});
-
-  ngOnInit() {
-    this.uploader.onAfterAddingFile = (file) => { file.withCredentials = false; }
-    this.uploader.onCompleteItem = (item: any, response: any, status: any, header: any) => {
-      console.log('FileUpload.uploaded:', item, status, response);
+  binaryData: any;
+  disassemblyData: any;
+  id = this.idService.id.subscribe(newId => {
+    console.log("here", newId)
+    if (newId == "") {
+      return;
     }
-  }
+    console.log("Firing!")
+    this.resolveBinary(newId);
+    this.resolveDisassembly(newId);
+    return newId;
+  });
 
-  public binary: any;
-  getBinary() {
-    // @ts-ignore
-    window.backend.binary('c279338c4e9a9b3b61317d32f8261c123d3c7d714d01b55c90ee43b07b05ad07').then(result => {
-      let buffer = new Buffer(result, 'base64');
-      console.log(buffer);
-      this.binary = hexdump(buffer);
+  constructor(
+    private loaderService: LoaderService,
+    private binaryService: BinaryService,
+    private disassemblyService: DisassemblyService,
+    private idService: IdService
+  ) {}
+
+  resolveBinary(id: string) {
+    this.loaderService.show();
+    this.binaryService.get(id).then(buffer => {
+      this.binaryData = buffer;
+      this.loaderService.hide();
     });
   }
 
-  public disassembly: any;
-  getDisassembly() {
-    // @ts-ignore
-    window.backend.disassembly('c279338c4e9a9b3b61317d32f8261c123d3c7d714d01b55c90ee43b07b05ad07').then(result => {
-      console.log(result);
-      this.disassembly = result;
-    });
-  }
-
-  public file: any;
-  getFile() {
-    // @ts-ignore
-    window.backend.sections('c279338c4e9a9b3b61317d32f8261c123d3c7d714d01b55c90ee43b07b05ad07').then(result => {
-      console.log(result);
-      this.file = result;
-    });
-  }
-  public extractedStrings: string[] = [];
-  getStrings() {
-    // @ts-ignore
-    window.backend.strings('c279338c4e9a9b3b61317d32f8261c123d3c7d714d01b55c90ee43b07b05ad07').then(result => {
-      console.log(result);
-      this.extractedStrings = result;
+  resolveDisassembly(id: string) {
+    this.loaderService.show();
+    this.disassemblyService.get(id).then(output => {
+      this.disassemblyData = output;
+      this.loaderService.hide();
     });
   }
 }
--- cmd/pippi/frontend/src/app/app.module.ts
+++ cmd/pippi/frontend/src/app/app.module.ts
@@ -1,25 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { APP_BASE_HREF } from '@angular/common';
 
-import { FileUploadModule } from 'ng2-file-upload';
 import { FormsModule } from '@angular/forms';
+import { FileUploadModule } from 'ng2-file-upload';
+
+import { AceEditorModule } from 'ng2-ace-editor';
+
+import { AngularSplitModule } from "angular-split";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
-import { APP_BASE_HREF } from '@angular/common';
+import { HexdumpComponent } from './hexdump/hexdump.component';
+import { ControlBarComponent } from './control-bar/control-bar.component';
+import { DisassemblyComponent } from './disassembly/disassembly.component';
+import { SelectIdComponent } from './select-id/select-id.component';
+import { LoaderComponent } from './loader/loader.component';
+import { LoaderService } from './loader.service';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HexdumpComponent,
+    ControlBarComponent,
+    DisassemblyComponent,
+    SelectIdComponent,
+    LoaderComponent,
   ],
   imports: [
+    AngularSplitModule.forRoot(),
     BrowserModule,
     FileUploadModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AceEditorModule
   ],
-  providers: [{provide: APP_BASE_HREF, useValue : '/' }],
+  providers: [LoaderService, {provide: APP_BASE_HREF, useValue : '/' }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
