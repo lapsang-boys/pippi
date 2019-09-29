d9d044591db36579cbc7457e6a066e09ae9367c7
Henry Eklind <henry@karlek.io>
2019-09-29 17:36:56 +0200

    feature(app): update main app view

--- cmd/pippi/frontend/src/app/app.component.ts
+++ cmd/pippi/frontend/src/app/app.component.ts
@@ -1,36 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { BinaryService } from './binary.service';
 import { DisassemblyService } from './disassembly.service';
 import { IdService } from './id.service';
 import { LoaderService } from './loader.service';
+import { StringsService } from './strings.service';
+import { SelectionService } from './selection.service';
 
 @Component({
   selector: '[id="app"]',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Pippi front-end';
 
   binaryData: any;
   disassemblyData: any;
-  id = this.idService.id.subscribe(newId => {
-    console.log("here", newId)
-    if (newId == "") {
-      return;
-    }
-    console.log("Firing!")
-    this.resolveBinary(newId);
-    this.resolveDisassembly(newId);
-    return newId;
-  });
-
+  stringsData: any;
+  disassemblyPos: number = 200;
   constructor(
+    private selectionService: SelectionService,
     private loaderService: LoaderService,
     private binaryService: BinaryService,
     private disassemblyService: DisassemblyService,
+    private stringsService: StringsService,
     private idService: IdService
   ) {}
+
+  ngOnInit() {
+    this.selectionService.selection.subscribe(newSelection => {
+      console.log(newSelection);
+    });
+
+    this.idService.id.subscribe(newId => {
+      if (newId == "") {
+        return;
+      }
+      this.resolveBinary(newId);
+      this.resolveDisassembly(newId);
+      this.resolveStrings(newId);
+      return newId;
+    });
+  }
 
   resolveBinary(id: string) {
     this.loaderService.show();
@@ -44,6 +55,14 @@ export class AppComponent {
     this.loaderService.show();
     this.disassemblyService.get(id).then(output => {
       this.disassemblyData = output;
+      this.loaderService.hide();
+    });
+  }
+
+  resolveStrings(id: string) {
+    this.loaderService.show();
+    this.stringsService.get(id).then(output => {
+      this.stringsData = output;
       this.loaderService.hide();
     });
   }
