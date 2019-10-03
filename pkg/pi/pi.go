91b23284653758ea9e925e4447142bca6e38ceee
Robin Eklind <rnd0x00@gmail.com>
2019-10-03 23:25:03 +0200

    pi: add BinDir and BinPath to resolve pippi cache paths
    Also, update cmd/pi-bin to make use of pi.BinPath.

--- pkg/pi/pi.go
+++ pkg/pi/pi.go
@@ -3,24 +3,54 @@ package pi
 
 import (
 	"crypto/sha256"
+	"os"
+	"path/filepath"
 	"strings"
 
 	"github.com/pkg/errors"
 )
 
 // CheckBinID validates the given binary ID.
-func CheckBinID(id string) error {
-	if len(id) != sha256.Size*2 {
-		return errors.Errorf("invalid length of binary ID; expected %d, got %d", sha256.Size*2, len(id))
+func CheckBinID(binID string) error {
+	if len(binID) != sha256.Size*2 {
+		return errors.Errorf("invalid length of binary ID; expected %d, got %d", sha256.Size*2, len(binID))
 	}
-	if id != strings.ToLower(id) {
-		return errors.Errorf("invalid binary ID; expected lowercase, got %q", id)
+	if binID != strings.ToLower(binID) {
+		return errors.Errorf("invalid binary ID; expected lowercase, got %q", binID)
 	}
 	const hex = "0123456789abcdef"
-	for _, r := range id {
+	for _, r := range binID {
 		if !strings.ContainsRune(hex, r) {
-			return errors.Errorf("invalid rune in binary ID; expected hexadecimal digit, got %q", r)
+			return errors.Errorf("invalid character in binary ID; expected hexadecimal digit, got %q", r)
 		}
 	}
 	return nil
+}
+
+// BinDir returns the project directory of the given binary ID.
+func BinDir(binID string) (string, error) {
+	if err := CheckBinID(binID); err != nil {
+		return "", errors.WithStack(err)
+	}
+	cacheDir, err := os.UserCacheDir()
+	if err != nil {
+		return "", errors.WithStack(err)
+	}
+	pippiCacheDir := filepath.Join(cacheDir, "pippi")
+	binDir := filepath.Join(pippiCacheDir, binID)
+	return binDir, nil
+}
+
+// Binary executable extension.
+const binExt = ".bin"
+
+// BinPath returns the file path to the binary of the given binary ID.
+func BinPath(binID string) (string, error) {
+	binDir, err := BinDir(binID)
+	if err != nil {
+		return "", errors.WithStack(err)
+	}
+	binName := binID + binExt
+	binPath := filepath.Join(binDir, binName)
+	return binPath, nil
 }
