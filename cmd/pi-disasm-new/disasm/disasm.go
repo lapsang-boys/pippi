099506ae524d79d7f1e1ec6544cac726810a8eb3
Robin Eklind <rnd0x00@gmail.com>
2019-10-05 11:44:17 +0200

    cmd/pi-disasm-new: include address in output

--- cmd/pi-disasm-new/disasm/disasm.go
+++ cmd/pi-disasm-new/disasm/disasm.go
@@ -2,15 +2,19 @@ package disasm
 
 import (
 	"fmt"
+
+	"github.com/decomp/exp/bin"
 )
 
 // Disassembler is a disassembler for a given machine architecture.
 type Disassembler interface {
 	// DecodeInst decodes the first instruction in buf.
-	DecodeInst(buf []byte) (Instruction, error)
+	DecodeInst(addr bin.Address, buf []byte) (Instruction, error)
 }
 
 // Instruction is an assembly instruction of a given machine architecture.
 type Instruction interface {
 	fmt.Stringer
+	// Addr returns the address of the instruction.
+	Addr() bin.Address
 }
